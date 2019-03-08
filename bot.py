29a217f666377ce17291c36946d78d8f943f2cfb
unknown <shivam.sansare99@gmail.com>
2019-03-08 22:03:51 +0530

    small trees

--- bot.py
+++ bot.py
@@ -1,21 +1,36 @@
+from PIL import ImageGrab, ImageOps
 import pyautogui
 import time
+from numpy import *
 
 class Cordinates():
-    replay = (480, 400)
-    dinosaur = (246, 405)
+    replayBtn = (340, 390)
+    dinosaur = (171, 395)
 
-def restart():
-    pyautogui.click(Cordinates.replay)
+def restartGame():
+    pyautogui.click(Cordinates.replayBtn)
 
-def space():
+def pressSpace():
     pyautogui.keyDown('space')
     time.sleep(0.05)
     print('Jump')
     pyautogui.keyUp('space')
 
+def image():
+    box = (Cordinates.dinosaur[0] + 60, Cordinates.dinosaur[1], Cordinates.dinosaur[0] + 100, Cordinates.dinosaur[1]+30)
+    image = ImageGrab.grab(box)
+    grayImage = ImageOps.grayscale(image)
+    a = array(grayImage.getcolors())
+    print(a.sum())
+    return(a.sum())
 
-restart()
-while True:
-    space()
-    time.sleep(0.05)
+
+def main():
+    restartGame()
+    while True:
+        if(image() != 1530):
+            pressSpace()
+            time.sleep(0.1)
+
+main()
+
