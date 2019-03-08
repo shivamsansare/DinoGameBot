423ae325e621c121a40c35e20df16e52e2953460
unknown <shivam.sansare99@gmail.com>
2019-03-08 22:10:17 +0530

    basic structure

--- bot.py
+++ bot.py
@@ -1,33 +1,21 @@
-from PIL import ImageGrab, ImageOps
 import pyautogui
 import time
-from numpy import *
 
 class Cordinates():
-    replay = (340, 390)
-    dinosaur = (171, 395)
+    replay = (480, 400)
+    dinosaur = (246, 405)
 
 def restart():
     pyautogui.click(Cordinates.replay)
 
-def press():
+def space():
     pyautogui.keyDown('space')
     time.sleep(0.05)
     print('Jump')
     pyautogui.keyUp('space')
 
-def image():
-    box = (Cordinates.dinosaur[0] + 60, Cordinates.dinosaur[1], Cordinates.dinosaur[0] + 100, Cordinates.dinosaur[1]+30)
-    image = ImageGrab.grab(box)
-    grayImage = ImageOps.grayscale(image)
-    a = array(grayImage.getcolors())
-    print(a.sum())
-    return(a.sum())
 
-restartGame()
+restart()
 while True:
-    if(image() != 1530):
-        pressSpace()
-        time.sleep(0.1)
-
-
+    space()
+    time.sleep(0.1)
